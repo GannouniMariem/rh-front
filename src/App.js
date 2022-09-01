f8732a66a27ae87d86e0528d25a7f69ee0225612
mariem gannouni <mariem.gannouni@symdrik.com>
2022-09-01 11:53:28 -0700

    login

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
 import React from 'react';
+import { Admin, Resource, ListGuesser,fetchUtils } from 'react-admin';
+
+
+import authProvider from './auth/authProvider';
+import myDataProvider from './auth/dataProvider';
 
 import Login from './Pages/auth/Login';
-
-import { Admin, Resource, ListGuesser,fetchUtils } from 'react-admin';
 import jsonServerProvider from 'ra-data-json-server';
 
-import {authProvider,httpClient} from './auth/authProvider';
 
 
-const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com', httpClient);
+const dataProvider = myDataProvider('https://jsonplaceholder.typicode.com');
     
 
 function App() {
   return (
 
-     <Admin loginPage={Login} dataProvider={dataProvider} authProvider={authProvider} requireAuth>
+     <Admin dataProvider={dataProvider} authProvider={authProvider} requireAuth>
           <Resource name="users" list={ListGuesser} />
     </Admin>
   );
